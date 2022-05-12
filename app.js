5308fdd4d816702d11de5ca55439a106ea2a59cd
ayindesegun <ayindesegun17@gmail.com>
2022-05-12 19:19:51 +0100

    Seccond changes

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const { default: mongoose } = require('mongoose')
-const encrypt = require('mongoose-encryption')
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const app = express()
-//console.log(process.env.SECRET)
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  }))
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 mongoose.connect('mongodb://localhost:27017/userDB')
 app.use(express.static('public'))
@@ -22,9 +31,13 @@ const userSchema = new mongoose.Schema({
   password: String,
 })
 
+userSchema.plugin(passportLocalMongoose);
 
-userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields: ['password']})
 const User = new mongoose.model('User', userSchema)
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -39,32 +52,10 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  })
-  newUser.save((err) => {
-    if (err) {
-      console.log(err)
-    } else {
-      res.render('secrets')
-    }
-  })
+  
 })
 app.post('/login', (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
-  User.findOne({ email: username }, (err, foundUser) => {
-    if (err) {
-      console.log(err)
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render('secrets')
-        }
-      }
-    }
-  })
+ 
 })
 app.get('/logout', (req, res) => {
   res.redirect('/')
